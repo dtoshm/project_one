23797ca5d947ebc066fe451c54f0d4e2bc566f1a
Dan McCahon <dtmccahon@outlook.com>
2023-07-28 13:43:30 -0400

    0.6 - Extra Credit

--- guessing_game.py
+++ guessing_game.py
@@ -9,10 +9,9 @@ import random
 def user_notification(message):
     print("-" * 15)
     print(message)
-    print("-" * 15)
 
 
-def start_game():
+def start_game(current_high_score):
     user_notification("Game Start: Please guess a number between 1 and 10.")
     # The game variables
     random_number = random.randint(1, 10)
@@ -36,10 +35,21 @@ def start_game():
             print("It's lower")
         elif user_guess < random_number:
             print("It's higher")
-    if number_of_guesses >= 5:
-        user_notification("You got it after {} gueses! :)\n Don't forget to pracice and thanks for playing!".format(number_of_guesses))
+
+    # Congratulate the user if they beat the high score
+    if number_of_guesses < current_high_score:
+        user_notification("Congrats!! you scored {} beating the original high score {}.".format(number_of_guesses, current_high_score)) 
+    elif number_of_guesses == current_high_score:
+        user_notification("Congrats!! you scored {} beating the matching the high score {}.".format(number_of_guesses, current_high_score))
     else:
-        user_notification("You got it after only {} gueses! :)\n Great Job and thanks for playing!".format(number_of_guesses))
+        user_notification("You scored {} therefore not beating the high score of {}.".format(number_of_guesses, current_high_score))         
+    # See if the user wants to play again
+    play_again = input("Would you like to play again? Y/N\n")
+    if play_again.upper() == "Y":
+        # Start game again
+        start_game(number_of_guesses)
+    else:
+        user_notification("Have a great day! Ending program.")
 
-
-start_game()
+# Start game and decide what the high score will be
+start_game(4)

34d2361cfeb8c4021bf3e36e767fb41126dd3786
Dan McCahon <dtmccahon@outlook.com>
2023-07-28 13:49:19 -0400

    0.7 - Comment consistency
    realized my comments weren't consistent with capitals

--- guessing_game.py
+++ guessing_game.py
@@ -15,18 +15,18 @@ def start_game(current_high_score):
     user_guess = 0
     while user_guess != random_number:
         try:
-            # check what the user enters is an integer for comparison
+            # Check what the user enters is an integer for comparison
             user_guess = int(input("What number would you like to guess? \n"))
             number_of_guesses += 1
-            # prevent user from guessing outside the range
+            # Prevent user from guessing outside the range
             if user_guess <= 0 or user_guess >= 11:
                 raise Exception("That number is not between 1 and 10, please try again.")
-        # prevent the user from entering numbers
+        # Prevent the user from entering numbers
         except ValueError:
             print("Please enter a number.")
         except Exception as e:
             print(e)        
-        # if the try block succeeds respond higher or lower
+        # If the try block succeeds respond higher or lower
         if user_guess > random_number:
             print("It's lower")
         elif user_guess < random_number:

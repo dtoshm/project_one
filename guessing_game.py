fca572446dd4a4f9a2da631b685046323b1ad788
Dan McCahon <dtmccahon@outlook.com>
2023-07-28 12:40:39 -0400

    0.4 - Dry & Functions
    Cleaned up the code and added a function to print user notifications.

--- guessing_game.py
+++ guessing_game.py
@@ -3,14 +3,16 @@ Python Development Techdegree
 Project 1 - The Number Guessing Game
 --------------------------------
 """
-
-# Import the random module.
 import random 
 
-# Write your code inside this function.
-def start_game():
+
+def user_notification(message):
     print("-" * 15)
-    print("Game Start: Please guess a number between 1 and 10.")
+    print(message)
+
+
+def start_game():
+    user_notification("Game Start: Please guess a number between 1 and 10.")
 
     random_number = random.randint(1, 10)
     number_of_guesses = 0
@@ -19,6 +21,7 @@ def start_game():
     while user_guess != random_number:
         try:
             user_guess = int(input("What number would you like to guess? \n"))
+            number_of_guesses += 1
             if user_guess <= 0 or user_guess >= 11:
                 raise Exception("That number is not between 1 and 10, please try again.")
         except ValueError:
@@ -30,19 +33,8 @@ def start_game():
             print("It's lower")
         elif user_guess < random_number:
             print("It's higher")
-        number_of_guesses += 1
-        
-    print("You got it!")
-    print("Game End: Thank you for playing!")
-    print("-" * 15)
+
+    user_notification("You got it after {} gueses! :)\n Thanks for playing!".format(number_of_guesses))
 
 
-
-#      and show how many attempts it took them to get the correct number.
-#   5. Let the player know the game is ending, or something that indicates the game is over.
-
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
 start_game()

8695a7ac1a854466f0b401b704cbf416e22d4514
Dan McCahon <dtmccahon@outlook.com>
2023-07-28 12:34:40 -0400

    0.3 - Step 4 and user input error handling
    Completed task 4 and added a try to handle user errors when entering a number.

--- guessing_game.py
+++ guessing_game.py
@@ -10,17 +10,34 @@ import random
 # Write your code inside this function.
 def start_game():
     print("-" * 15)
-    num = random.randint(1, 10)
-    user_guess = input(int("Guess a number"))
-    
+    print("Game Start: Please guess a number between 1 and 10.")
+
+    random_number = random.randint(1, 10)
+    number_of_guesses = 0
+    user_guess = 0
+
+    while user_guess != random_number:
+        try:
+            user_guess = int(input("What number would you like to guess? \n"))
+            if user_guess <= 0 or user_guess >= 11:
+                raise Exception("That number is not between 1 and 10, please try again.")
+        except ValueError:
+            print("Please enter a number.")
+        except Exception as e:
+            print(e)        
+
+        if user_guess > random_number:
+            print("It's lower")
+        elif user_guess < random_number:
+            print("It's higher")
+        number_of_guesses += 1
+        
+    print("You got it!")
+    print("Game End: Thank you for playing!")
+    print("-" * 15)
 
 
 
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
-
-#   4. Once the guess is correct, stop looping, inform the user they "Got it"
 #      and show how many attempts it took them to get the correct number.
 #   5. Let the player know the game is ending, or something that indicates the game is over.
 
